1b749fb54b33213701e649ab6394a8dad394ad0b
micronn <nanami_tanida@rebuild-inc.co.jp>
2025-02-10 13:08:35 +0900

    [update] stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
 
-export default function RootLayout() {
-  return <Stack />;
+export default function Layout() {
+    return (
+        <Stack
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: '#f4511e',
+                },
+                headerTintColor: '#fff',
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                },
+            }}>
+            {/* Optionally configure static options outside the route.*/}
+            <Stack.Screen name="home" options={{}} />
+        </Stack>
+    );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,41 @@
-import { Text, View } from "react-native";
+import { Link, Stack } from 'expo-router';
+import { Image, Text, View, StyleSheet } from 'react-native';
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+function LogoTitle() {
+    return (
+        <Image style={styles.image} source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} />
+    );
 }
+
+export default function Home() {
+    return (
+        <View style={styles.container}>
+            <Stack.Screen
+                options={{
+                    title: 'My home',
+                    headerStyle: { backgroundColor: '#f4511e' },
+                    headerTintColor: '#fff',
+                    headerTitleStyle: {
+                        fontWeight: 'bold',
+                    },
+
+                    headerTitle: props => <LogoTitle />,
+                }}
+            />
+            <Text>Home Screen</Text>
+            <Link href={{ pathname: '/details', params: { name: 'Bacon' } }}>Go to Details</Link>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    image: {
+        width: 50,
+        height: 50,
+    },
+});
