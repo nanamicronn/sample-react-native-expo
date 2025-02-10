61b81b137028ce5ff74c5788a69fb3c71cc62fde
micronn <nanami_tanida@rebuild-inc.co.jp>
2025-02-10 13:14:22 +0900

    [update] tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,9 @@
-import { Stack } from 'expo-router';
+import { Stack } from 'expo-router/stack';
 
 export default function Layout() {
     return (
-        <Stack
-            screenOptions={{
-                headerStyle: {
-                    backgroundColor: '#f4511e',
-                },
-                headerTintColor: '#fff',
-                headerTitleStyle: {
-                    fontWeight: 'bold',
-                },
-            }}>
-            {/* Optionally configure static options outside the route.*/}
-            <Stack.Screen name="home" options={{}} />
+        <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         </Stack>
     );
 }
